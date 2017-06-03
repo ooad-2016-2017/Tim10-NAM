e280c4995134c6f0e6d607b3da3a26b3971d9d4a
adnatucak <atucak1@etf.unsa.ba>
2017-06-04 00:37:32 +0200

    Assets dodan

---
+++ IgricaNAM/Assets/Assets/Scripts/Restart.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Restart : MonoBehaviour {
+	public void RestartGame()
+	{
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
